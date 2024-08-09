56bdecac2b3dcb3e7210142c490c9fd022a6d475
Dmitry Bastrygin <dima.bass@mail.ru>
2024-08-09 19:37:22 +0300

    initialize project

---
+++ src/test/java/ru/dbastrygin/shawarmacloud/ShawarmaCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dbastrygin.shawarmacloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShawarmaCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
